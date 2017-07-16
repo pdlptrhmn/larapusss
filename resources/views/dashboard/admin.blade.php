80b5921f8de12604ce14a2b83e408e569fb73f27
151610222-smkassalaambandung <151610222@smkassalaambandung.sch.id>
2017-07-16 11:52:30 +0700

    parantoss

---
+++ resources/views/dashboard/admin.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Dashboard</h2>
+                </div>
+
+                <div class="panel-body">
+                    Selamat Datang Di Menu Administrasi Larapus Silahkan Pilih Menu Administrasi yang diinginkan.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
